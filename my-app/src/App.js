de461b9073f5348039924b8e4bc7411227957eee
yeondon <s25030@gsm.hs.kr>
2025-07-14 17:14:08 +0900

    버튼 눌렀을떄 +1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
-let post = 0;
 function App() {
+  const [post, setPost] = useState(0);
+  const handleClick = () => {
+    setPost(post + 1);
+  };
   return (
     <div>
-      <div className="flex justify-center items-center w-full h-screen text-3xl ">
-        안녕! 나는 Tailwind 스타일이야.
+      <div className="flex justify-center items-center w-full h-screen text-3xl flex-col">
+        <div>
+          <button
+            onClick={handleClick}
+            className="border-2 border-black bg-black text-white p-[10px]"
+          >
+            클릭하세요
+          </button>
+        </div>
+        <div>{post}</div>
       </div>
     </div>
   );
