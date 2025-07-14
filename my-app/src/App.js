3c729d1b8b5b0edd79192c800767b38dde8f4f8f
yeondon <s25030@gsm.hs.kr>
2025-07-14 15:40:53 +0900

    세팅 완료

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+let post = 0;
+function App() {
+  return (
+    <div>
+      <div className="flex justify-center items-center w-full h-screen text-3xl ">
+        안녕! 나는 Tailwind 스타일이야.
+      </div>
+    </div>
+  );
+}
+
+export default App;
